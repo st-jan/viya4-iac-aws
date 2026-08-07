370616c895043ca8dba35fc3658b692d0934dc91
Jan Stienstra <jan.stienstra@sas.com>
2026-08-07 14:36:49 +0200

    Remove EBS driver. This will be installed by the deployment as code project

--- main.tf
+++ main.tf
@@ -191,10 +191,6 @@ module "eks" {
     coredns = {
       most_recent = true
     }
-    aws-ebs-csi-driver = {
-      most_recent              = true
-      service_account_role_arn = module.ebs.ebs_csi_account
-    }
   }
 
   ## Any individual Node Group customizations should go here
